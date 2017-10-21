633bdb1de67c2c72fcef411144e64596e502bf0c
Jay Stockhausen <jay.stockhausen@gmail.com>
2017-10-21 23:35:34 +0000

    Initial eslint setup

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
-var express = require('express');
-var mongoClient = require('mongodb').MongoClient;
-var fs = require('fs');
+"use strict";
 
-var app = express();
-var creds = fs.readFileSync(__dirname + '/mongodb.creds');
-var jsonContent = JSON.parse(creds);
-var user = jsonContent.user;
-var pwd = jsonContent.pwd;
-var server = jsonContent.server;
-var mongoUrl = 'mongodb://' + user + ':' + pwd + '@' + server + '/heroes';
-var views = __dirname + '/views/';
+const express = require('express');
+const mongoClient = require('mongodb').MongoClient;
+const fs = require('fs');
+const path = require('path');
 
-app.get('/', (req, res, next) => {
-	res.sendFile(views + 'add_movie.html');
+const app = express();
+const creds = fs.readFileSync(path.join(__dirname, 'mongodb.creds'));
+const jsonContent = JSON.parse(creds);
+const user = jsonContent.user;
+const pwd = jsonContent.pwd;
+const server = jsonContent.server;
+const mongoUrl = 'mongodb://' + user + ':' + pwd + '@' + server + '/heroes';
+const views = path.join( __dirname, 'views');
+
+app.get('/',function(req, res, next){
+	res.sendFile(views+'/add_movie.html');
 });
+
 
 app.listen(8080, (req, res, next) => console.log("Listening on port 8080"));

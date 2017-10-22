43446f814625b66d7cddc99c5e5cbd32603a2f37
Jay Stockhausen <jay.stockhausen@gmail.com>
2017-10-22 04:35:26 +0000

    Formatting changes

--- app.js
+++ app.js
@@ -1,22 +1,40 @@
-"use strict";
-
-const express = require('express');
-const mongoClient = require('mongodb').MongoClient;
-const fs = require('fs');
-const path = require('path');
+// Import modules
+const express = require("express"),
+  mongoClient = require("mongodb").MongoClient,
+  fs = require("fs"),
+  path = require("path"),
+  bodyParser = require("body-parser"),
+  assert = require("assert");
 
 const app = express();
-const creds = fs.readFileSync(path.join(__dirname, 'mongodb.creds'));
+app.use(bodyParser.urlencoded({ extended: true }));
+const creds = fs.readFileSync(path.join(__dirname, "mongodb.creds"));
 const jsonContent = JSON.parse(creds);
 const user = jsonContent.user;
 const pwd = jsonContent.pwd;
 const server = jsonContent.server;
-const mongoUrl = 'mongodb://' + user + ':' + pwd + '@' + server + '/heroes';
-const views = path.join( __dirname, 'views');
+const mongoUrl = "mongodb://" + user + ":" + pwd + "@" + server + "/video";
+const views = path.join(__dirname, "views");
 
-app.get('/',function(req, res, next){
-	res.sendFile(views+'/add_movie.html');
-});
-
-
-app.listen(8080, (req, res, next) => console.log("Listening on port 8080"));
+mongoClient
+  .connect(mongoUrl)
+  .then(db => {
+    console.log("Successful connection to MongoDB!");
+    app.get("/", function(req, res) {
+      res.sendFile(views + "/add_movie.html");
+      //res.render('add_movie', {});
+    });
+    app.post("/add_movie", function(req, res) {
+      let title = req.body.title;
+      let year = req.body.year;
+      let imdb = req.body.imdb;
+      db
+        .collection("movies")
+        .insertOne({ title: title, year: year, imdb: imdb }, function(err, r) {
+          assert.equal(null, err);
+          res.send("Document inserted with _id: " + r.insertedId);
+        });
+    });
+    app.listen(8080, (req, res) => console.log("Listening on port 8080"));
+  })
+  .catch(err => console.log(err));
